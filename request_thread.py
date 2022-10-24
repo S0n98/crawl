8bb108a4301dbe4d0ae7e687d7e3ad9dedd7cff1
S0n98 <danghs198@gmail.com>
2022-10-24 07:19:40 +0700

    init

--- request_thread.py
+++ request_thread.py
@@ -1,6 +1,9 @@
 import threading
 import requests
 from bs4 import BeautifulSoup
+from lxml import html
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
 
 class RequestThread(threading.Thread):
   def __init__(self, url):
@@ -10,11 +13,14 @@ class RequestThread(threading.Thread):
   
   def run(self):
     try:
-      headers = {'Accept-Encoding': 'gzip'}
+      headers = {'Accept-Encoding': 'gzip, deflate, br', 'User-Agent': user_agent, 'accept': accept}
       res = requests.get(self.url, timeout=5, headers=headers)
-      print(res.text)
 
-      soup = BeautifulSoup(res.text, "html.parser")
-      self.result = {self.url.split(':')[-1]:soup.body.get_text().strip()}
+      tree = html.fromstring(res.content)
+      print(tree)
+      #needed_data = tree.xpath('/html/body/div/div[2]/table/tbody/tr[20]/td[2]/ul')
+      needed_data = tree.xpath('/html/text()')
+      print(needed_data)
+      self.result = {self.url.split(':')[-1]:needed_data}
     except(requests.exceptions.Timeout):
       print(f"{self.url} timeout ...")
